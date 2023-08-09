d917a103c9fc0e89e7e2253e922983c50c878081
Metehan ATAKAN <m.atakakan@diten.tech>
2023-08-10 02:01:53 +0300

    create project

---
+++ components/Header/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <Link>
+            </Link>
+
+            <nav>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
